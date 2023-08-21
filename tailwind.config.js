1a084900535faa1e9d2438108e3d846eeaf7ff42
Stefanos Michelakis <stefanosmichelakis@gmail.com>
2023-08-21 09:33:49 +0300

    add betterhover to disable hover on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,26 @@ module.exports = {
       md: '8px',
       xl: '16px',
     },
+    screens: {
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      'min-lg': { min: '1023px' },
+
+      betterhover: { raw: '(hover: hover)' },
+    },
   },
   plugins: [],
 }
