eaeb9ffea26d4b0255f1ea90da7015bfc7497ac8
StevenKDL <s.michelakis@kdevlabs.com>
2024-04-05 12:06:58 +0300

    Update work status

--- modules/common.ts
+++ modules/common.ts
@@ -51,8 +51,9 @@ export const workExperience: Array<WorkExperienceType> = [
     jobTitle: 'Full Stack Software Engineer',
     location: 'Remote',
     description:
-      'I am currently working as a Full Stack Software engineer, working in a team that maintains code and develops features for a web platform with thousands of daily active users.',
+      'In this role I was collaborating with other developers and testers in a team that maintained and developed features for a web platform with thousands of daily active users.',
     dateStarted: new Date('05/15/2023'),
+    dateEnded: new Date('09/30/2023'),
     responsibilities: [
       'Develop HTML/CSS/JavaScript code as a front-end Engineer',
       'Design and implement APIs with NodeJS and ExpressJS',
@@ -90,6 +91,20 @@ export const workExperience: Array<WorkExperienceType> = [
       'Troubleshoot hardware and software',
       'Cooporate with my supervisor while working on solutions',
       'Communicate with coleagues and walk them through solutions to their technical problems',
+    ],
+  },
+  {
+    employer: 'Kanellis Development Labs',
+    employerLink: 'https://kdevlabs.com/',
+    jobTitle: 'Full Stack Software Engineer',
+    location: 'Remote',
+    description:
+      '',
+    dateStarted: new Date('10/01/2023'),
+    responsibilities: [
+      'Maintain and develop code for an android app that utilizes a webview application which is based on HTML/CSS and JavaScript. More specifically, the front end runs mostly on Vanilla HTML/CSS and JavaScript while the backend runs on an ExpressJS API that retrieves and modifies data from a MySQL database',
+      'Research and develop Angular code utilizing RxJS and PrimeNG libraries while keeping documentation for future usages',
+      'Development of a mobile application portal based on React Native while utilizing a simple Google Firebase RTDB back end',
     ],
   },
 ];
